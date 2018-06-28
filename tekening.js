09451f8b748a5a52b103fe205cf0837a8cda30ca
ziggykruisselbrink <38703793+ziggykruisselbrink@users.noreply.github.com>
2018-06-28 14:05:47 +0200

    Update tekening.js

--- tekening.js
+++ tekening.js
@@ -1,30 +1,65 @@
-var xCar = 50;
+
+var xGuy;
+
+var speed;
+ 
+
 
 function setup() {
-  //Canvas van 200 px bij 200 px
-  createCanvas(200, 180); 
-  //Één cijfer geeft grijswaarden
-  background(220); 
+createCanvas(400, 300); 
+xGuy = 100;
+speed = 2
 }
 
 function draw() {
-    //Geen randen om de auto
-    noStroke(); 
-    //Kies de opvulkleur (rgb)
-    fill(255, 220, 115); 
-    //Parameters: x, y, breedte, hoogte
-    rect(xCar, 100, 110, 20); 
-    rect(xCar + 10, 78, 70, 40);
-    // Wielen
-    //Witte randen voor de wielen
-    stroke(255); 
-    //Iets dikkere randen
-    strokeWeight(2); 
-    fill(12, 66, 66);
-    //Parameters: x, y, breedte, hoogte
-    ellipse(xCar + 25, 121, 24, 24); 
-    ellipse(xCar + 80, 121, 24, 24);
-    stroke(0); //Zwarte weg
-    //Parameters: x1,y1,x2,y2 
-    line(0, 121+12, 200, 121+12);
+
+  document.body.style.backgroundColor = "#821717";
+
+
+ellipseMode(CENTER);
+rectMode(CENTER);
+background(255);
+
+ 
+
+fill(150, 0, 150);
+rect(xGuy, 150, 45, 95);
+
+ 
+
+fill(255, 0, 0, 220);
+ellipse(xGuy, 115, 60, 60);
+
+
+  fill(200);
+  ellipse(xGuy, 130, 30, 12);
+
+ 
+
+fill(0, 0, 255);
+ellipse(xGuy - 17, 110, 20, 22);
+ellipse(xGuy + 17, 110, 20, 22);
+
+ 
+ 
+
+line(xGuy - 10, 195, xGuy - 20, 235);
+line(xGuy + 10, 195, xGuy + 20, 235);
+
+
+  line(xGuy - 21, 150, xGuy - 60, 139);
+  line(xGuy + 21, 150, xGuy + 60, 139);
+  
+  
+  line(0, 235, 500, 235)
+  line(0, 237, 500, 237)
+  line(0, 236, 500, 236)
+  
+  xGuy += 1;
+  
+          if (xGuy > width || xGuy < 0){ 
+        speed = -speed;
+    }
+    xGuy += speed;
+}
 };
